abb9fd2c854adba745aaac28b62b8b903070c05b
dirgepye <s.stenhouse87@gmail.com>
2016-02-25 19:49:34 +0000

    Implementing test JS to actual project

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-    $('form').on('submit', (function() {
-        var input1 = document.getElementById("input1").value;
-        var input2 = document.getElementById("input2").value;
-        var input3 = document.getElementById("input3").value;
-        console.log("after a long " + input1 + " at the beach, John decided to get a " + input2 + " and then went to get a " + input3 + " haircut")
-    }));
+    $('#main').html('<h2>fdsafdgagd<h2>')
+    $('#main').append("<h2>fdsaf</h2>")
+    
+    // $('form').on('submit', (function() {
+    //     var input1 = document.getElementById("input1").value;
+    //     var input2 = document.getElementById("input2").value;
+    //     var input3 = document.getElementById("input3").value;
+    //     $('#main').html('');
+    //     $('#main').append("<p>after a long <span style='color: blue'>" + input1 + "</span> at the beach, John decided to get a " + input2 + " and then went to get a " + input3 + " haircut</p>")
+    // }));
 });
